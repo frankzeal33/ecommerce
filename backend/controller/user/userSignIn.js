28657f4bb896bee9fbe6cb4dcf5ceba6536cb776
frankzeal33 <frankzeal33@gmail.com>
2025-01-09 23:47:34 +0100

    second commit

--- backend/controller/user/userSignIn.js
+++ backend/controller/user/userSignIn.js
@@ -28,7 +28,7 @@ async function userSignInController(req,res){
             _id : user._id,
             email : user.email,
         }
-        const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET_KEY, { expiresIn: 60 * 60 * 8 });
+        const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET_KEY, { expiresIn: "1d"});
 
         const tokenOption = {
             httpOnly : true,

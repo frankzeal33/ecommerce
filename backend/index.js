ceb756cf20e5d2ee05c80a1c31031920df590279
frankzeal33 <frankzeal33@gmail.com>
2025-01-30 14:29:55 +0100

    server edited

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(cookieParser())
 app.use(cors({
     origin : process.env.FRONTEND_URL,
     credentials : true,
-    exposedHeaders: ["set-cookie"]
+    // exposedHeaders: ["set-cookie"]
 }))
 
 
